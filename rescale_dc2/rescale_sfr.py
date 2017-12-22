f671bb036d2ca0c4e5e8b2a4590ba49e1397ab7b
Andrew Hearin <andrew.hearin@yale.edu>
2017-12-22 14:41:50 -0600

    Updated ssfr devel notebook

---
+++ rescale_dc2/rescale_sfr.py
@@ -0,0 +1,11 @@
+"""
+"""
+import numpy as np
+from halotools.empirical_models import conditional_abunmatch
+
+
+def remap_ssfr(protoDC2, universe_machine, logsm_bins=np.linspace(9, 12, 25)):
+    """
+    """
+    raise NotImplementedError
+
