95029e08cdcb59da393880e33730bb2caf4495d0
Andrew Hearin <andrew.hearin@yale.edu>
2018-01-18 10:17:08 -0600

    Fixed filename bug

--- rescale_dc2/create_value_added_catalogs.py
+++ rescale_dc2/create_value_added_catalogs.py
@@ -1,5 +1,6 @@
 """
 """
+import os
 from slidingpercentile import sliding_percentile
 from .load_halos import load_bolshoi_planck_halos
 from .load_umachine_mocks import load_umachine_and_value_added_halos
@@ -14,8 +15,13 @@ def store_value_added_umachine_bolshoi_catalogs(umachine_fname, bolshoi_halos_fn
     window = 501
     umachine['sfr_percentile_fixed_sm'] = sliding_percentile(x, y, window)
 
-    outname_umachine = "value_added_" + umachine_fname
-    outname_bolshoi = "value_added_" + bolshoi_halos_fname
+    umachine_basename = os.path.basename(umachine_fname)
+    output_umachine_basename = "value_added_" + umachine_basename
+    outname_umachine = os.path.join(os.path.dirname(umachine_fname), output_umachine_basename)
+
+    bolshoi_basename = os.path.basename(bolshoi_halos_fname)
+    output_bolshoi_basename = "value_added_" + bolshoi_basename
+    outname_bolshoi = os.path.join(os.path.dirname(bolshoi_halos_fname), output_bolshoi_basename)
 
     umachine.write(outname_umachine, path='data')
     bpl_halos.write(outname_bolshoi, path='data')
