c3bdc5283494d7b34c81e9ce32447c91fba6fae3
Andrew Hearin <andrew.hearin@yale.edu>
2018-01-18 14:53:02 -0600

    Added new sdss_kd_tree_indices function in new module

---
+++ rescale_dc2/matching_sdss_galaxy_selection.py
@@ -0,0 +1,18 @@
+"""
+"""
+import numpy as np
+from scipy.spatial import cKDTree
+
+
+__all__ = ('sdss_kd_tree_indices', )
+
+
+def sdss_kd_tree_indices(mstar_mock, sfr_percentile_mock, logsm_sdss, sfr_percentile_sdss):
+    """
+    """
+    sdss_tree = cKDTree(np.vstack((np.log10(logsm_sdss), sfr_percentile_sdss)).T)
+
+    nn_distinces, nn_indices = sdss_tree.query(
+        np.vstack((np.log10(mstar_mock), sfr_percentile_mock)).T, k=1)
+
+    return nn_distinces, nn_indices
