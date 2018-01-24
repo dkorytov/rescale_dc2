14f4404fb747ea8d665dfa023dfad6e877e69634
Andrew Hearin <andrew.hearin@yale.edu>
2018-01-24 14:18:32 -0600

    Added implementation of source_halo_selection_indices

--- rescale_dc2/generate_snapshot_collection.py
+++ rescale_dc2/generate_snapshot_collection.py
@@ -3,6 +3,7 @@
 import numpy as np
 from astropy.table import Table
 from scipy.spatial import cKDTree
+from .nearest_umachine_halo_selection import source_halo_selection_indices
 
 
 def load_umachine_z0p1_color_mock(umachine_z0p1_color_mock_fname):
@@ -108,18 +109,19 @@ def write_sdss_restframe_color_snapshot_mocks_to_disk(
 
         #  For every host halo in the halo catalog hosting the protoDC2 galaxies,
         #  find a matching halo in halo catalog hosting the UniverseMachine galaxies
-        source_halo_selection_indices = calculate_source_halo_selection_indices(
-            bolshoi_planck_halo_catalog, protoDC2_fof_halo_catalog)
+        source_halo_indx = source_halo_selection_indices(
+            bolshoi_planck_halo_catalog['log10_cumulative_nd_mvir'],
+            protoDC2_fof_halo_catalog['log10_cumulative_nd_mvir'])
 
         #  Calculate the indices of the UniverseMachine galaxies that will be selected
         #  find a matching halo in halo catalog hosting the UniverseMachine galaxies
-        source_galaxy_selection_indices = calculate_source_galaxy_selection_indices(
+        source_galaxy_indx = calculate_source_galaxy_selection_indices(
             bolshoi_planck_halo_catalog, protoDC2_fof_halo_catalog)
 
         #  Assemble the output protoDC2 mock
         output_snapshot_mock = build_output_snapshot_mock(
             umachine_mstar_ssfr_mock_with_colors, protoDC2_fof_halo_catalog,
-            source_halo_selection_indices, source_galaxy_selection_indices)
+            source_halo_indx, source_galaxy_indx)
 
         #  Write the output protoDC2 mock to disk
         output_snapshot_mock.write(output_color_mock_fname, path='data', overwrite=overwrite)
