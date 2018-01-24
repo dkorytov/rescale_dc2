90b875d94c7a4bbde46dac926238e08222be20e7
Andrew Hearin <andrew.hearin@yale.edu>
2018-01-24 13:23:12 -0600

    Finished skeleton of algorithm for write_sdss_restframe_color_snapshot_mocks_to_disk

--- rescale_dc2/generate_snapshot_collection.py
+++ rescale_dc2/generate_snapshot_collection.py
@@ -27,10 +27,36 @@ def load_bolshoi_planck_halo_catalog(bolshoi_planck_halo_catalog_fname):
     return Table.read(bolshoi_planck_halo_catalog_fname, path='data')
 
 
+def transfer_colors_to_umachine_mstar_ssfr_mock(umachine_mstar_ssfr_mock, umachine_z0p1_color_mock):
+    """
+    """
+    raise NotImplementedError()
+    return umachine_mstar_ssfr_mock
+
+
+def calculate_source_halo_selection_indices(bolshoi_planck_halo_catalog, protoDC2_fof_halo_catalog):
+    """
+    """
+    raise NotImplementedError()
+
+
+def calculate_source_galaxy_selection_indices(protoDC2_fof_halo_catalog, source_halo_selection_indices):
+    """
+    """
+    raise NotImplementedError()
+
+
+def build_output_snapshot_mock(umachine_mstar_ssfr_mock_with_colors, protoDC2_fof_halo_catalog,
+            source_halo_selection_indices, source_galaxy_selection_indices):
+    """
+    """
+    raise NotImplementedError()
+
+
 def write_sdss_restframe_color_snapshot_mocks_to_disk(
             umachine_z0p1_color_mock_fname, protoDC2_fof_halo_catalog_fname_list,
             umachine_mstar_ssfr_mock_fname_list, bolshoi_planck_halo_catalog_fname_list,
-            output_color_mock_fname_list):
+            output_color_mock_fname_list, overwrite=False):
     """
     """
     umachine_z0p1_color_mock = load_umachine_z0p1_color_mock(umachine_z0p1_color_mock_fname)
@@ -43,9 +69,17 @@ def write_sdss_restframe_color_snapshot_mocks_to_disk(
         protoDC2_fof_halo_catalog = load_protoDC2_fof_halo_catalog(fname2)
         bolshoi_planck_halo_catalog = load_bolshoi_planck_halo_catalog(fname3)
 
-        raise NotImplementedError()
+        umachine_mstar_ssfr_mock_with_colors = transfer_colors_to_umachine_mstar_ssfr_mock(
+            umachine_mstar_ssfr_mock, umachine_z0p1_color_mock)
 
+        source_halo_selection_indices = calculate_source_halo_selection_indices(
+            bolshoi_planck_halo_catalog, protoDC2_fof_halo_catalog)
 
+        source_galaxy_selection_indices = calculate_source_galaxy_selection_indices(
+            bolshoi_planck_halo_catalog, protoDC2_fof_halo_catalog)
 
+        output_snapshot_mock = build_output_snapshot_mock(
+            umachine_mstar_ssfr_mock_with_colors, protoDC2_fof_halo_catalog,
+            source_halo_selection_indices, source_galaxy_selection_indices)
 
-
+        output_snapshot_mock.write(output_color_mock_fname, path='data', overwrite=overwrite)
